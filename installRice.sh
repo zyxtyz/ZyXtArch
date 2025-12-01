55453adb2b4d9b08a742f44f79973841af241300
zyxtyz <zyxtyz@proton.me>
2025-12-01 14:12:49 +0200

    fixed installRice.sh

--- installRice.sh
+++ installRice.sh
@@ -1,14 +1,14 @@
 #!/bin/sh 
 
 echo "==> Installing git and required packages..."
-sudo pacman -S --noconfirm --needed git bspwm sxhkd feh
+sudo pacman -S --noconfirm --needed git bspwm sxhkd feh >/dev/null 2>&1
 
 echo "==> Cloning ZyXtArch repo..."
 cd "$HOME"
 
-git clone https://github.com/zyxtyz/ZyXtArch
-mkdir ~/.config
-mv "$HOME/ZyXtArch" "$HOME/.config/zyxtarch"
+git clone https://github.com/zyxtyz/ZyXtArch >/dev/null 2>&1
+mkdir ~/.config >/dev/null 2>&1
+cp -r "$HOME/ZyXtArch" "$HOME/.config/zyxtarch" >/dev/null 2>&1
 
 echo "==> Creating bspwm-zyxtarch session file..."
 sudo mkdir -p /usr/share/xsessions/
@@ -21,28 +21,33 @@ Type=Application
 DesktopNames=bspwm
 EOF
 
-if command -v paru then	
-	echo "Paru is installed!"
+echo "==> Checking if paru is installed..."
+if command -v paru >/dev/null 2>&1; then
+  echo "Installed!"
 else
-echo "==> Installing paru (AUR helper)..."
-sudo pacman -S --noconfirm --needed base-devel
-cd "$HOME"
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si --noconfirm
-cd "$HOME"
-rm -rf paru
-
+  echo "==> Paru not installed.."
+  echo "==> Installing paru (AUR helper)..."
+  sudo pacman -S --noconfirm --needed base-devel
+  cd "$HOME"
+  git clone https://aur.archlinux.org/paru.git
+  cd paru
+  makepkg -si --noconfirm >/dev/null 2>&1
+  cd "$HOME"
+  rm -rf paru 
 fi
-echo "==> Adding custom binaries to /usr/local/bin..."
-cd "$HOME/.config/zyxtarch/bin"
-sudo cp rice /usr/local/bin/
-sudo cp tui-pacman /usr/local/bin/
+
+
 
 echo "==> Installing additional packages with paru..."
 paru -S --noconfirm flameshot fzf neovim cava kitty wallust-git \
   xdg-desktop-portal-wlr-git xdg-utils xorg-init xorg-server zsh \
   eww-git nushell zinit-git mpd mpc ncmpcpp yt-dlp bc nerd-fonts
+
+echo "==> Setting up zsh..."
+touch ~/.zshrc
+echo "source ~/.config/zyxtarch/zsh/.zshrc" > ~/.zshrc
+chsh -s /bin/zsh
+
 
 mkdir ~/Music
 
@@ -55,6 +60,7 @@ rice font $HOME/CartographCF-BoldItalic.otf
 
 
 echo "==> Cleaning..."
-rm -rf $HOME/{CartographCF-BoldItalic.otf,ZyXtArch}
+rm -rf $HOME/*
+mkdir $HOME/{Music,Downloads}
 echo "==> Setup complete! Rebooting system..."
 sudo reboot
